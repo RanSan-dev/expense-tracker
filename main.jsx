f35af95512e6aa2680f7d55b057c629060d9e127
RanSan-dev <randywins@comcast.net>
2026-04-02 10:43:02 -0700

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,1 +1,5 @@
-import "./App.jsx";
+<script src="https://unpkg.com/react@18/umd/react.development.js"></script>
+<script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
+<script src="https://cdn.tailwindcss.com"></script>
+
+<script type="module" src="/main.jsx"></script>
